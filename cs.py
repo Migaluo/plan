7944ba987b26a3fd241400636f20f0dce3f9cfd5
realMiga <34860804+realMiga@users.noreply.github.com>
2018-01-22 17:12:13 +0800

    Add files via upload

--- cs.py
+++ cs.py
@@ -1,27 +1,3 @@
-import random
-
-
-def fun():
-    choose = input('your choose:(B/S)')
-    flag = False
-    point1 = random.randrange(1, 7)
-    point2 = random.randrange(1, 7)
-    point3 = random.randrange(1, 7)
-    print('The point is: ')
-    print(point1, point2, point3)
-    if 11 <= (point1 + point2 + point3) <= 18:
-        flag = not flag
-    else:
-        flag = flag
-
-    if flag and choose == 'B':
-        return 'you win'
-    if not flag and choose == 'S':
-        return 'you win'
-    return 'you lose'
-
-
-count = 0
-while count < 3:
-    count += 1
-    print(fun())
+def normalize(name):
+    return name [s0].upper() + name [s1:].lower()
+print (map (normalize,'adam','LISA','barT'))
